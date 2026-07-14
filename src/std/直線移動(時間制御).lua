5b9fc7be68384203b8459cf36b340f7e965ad6fd
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-07-14 12:02:56 +0900

    feat: 直線移動(時間制御)のオーバーシュートに対応

--- src/std/直線移動(時間制御).lua
+++ src/std/直線移動(時間制御).lua
@@ -1,13 +1,48 @@
 --speed:0,0
 --timecontrol
 
-local curves = obj.module("enhanced_tracks.aux2")
-
-local index, ratio = math.modf(obj.getpoint("timecontrol", "index"))
 local num = obj.getpoint("num")
+assert(num > 0, "trackbar movement requires at least one point")
+
 local values = {}
 for i = 0, num - 1 do
   values[i + 1] = obj.getpoint(i)
 end
 
-return curves.linear_value(values, index, ratio, obj.getpoint("accelerate"), obj.getpoint("decelerate"))
+if num == 1 then
+  return values[1]
+end
+
+local position = obj.getpoint("timecontrol", "index")
+local segment
+local ratio
+if position < 0.0 then
+  segment = 0
+  ratio = position
+elseif position >= num - 1 then
+  segment = num - 2
+  ratio = position - segment
+else
+  segment, ratio = math.modf(position)
+end
+
+local start_value = values[segment + 1]
+local end_value = values[segment + 2]
+local accelerate = segment == 0 and obj.getpoint("accelerate")
+local decelerate = segment == num - 2 and obj.getpoint("decelerate")
+if not accelerate and not decelerate then
+  return start_value + (end_value - start_value) * ratio
+end
+
+local average = (start_value + end_value) * 0.5
+local previous_control = average
+if accelerate then
+  previous_control = start_value
+end
+local next_control = average
+if decelerate then
+  next_control = end_value
+end
+local reverse_ratio = 1.0 - ratio
+return (previous_control * ratio * 3.0 + reverse_ratio * start_value) * reverse_ratio * reverse_ratio
+  + (next_control * reverse_ratio * 3.0 + ratio * end_value) * ratio * ratio
